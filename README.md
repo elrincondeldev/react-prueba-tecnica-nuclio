928598843eaf0497ca06aace70328b291c493a17
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-07-15 20:12:25 +0200

    react + tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+# Prueba técnica React + TailwindCSS
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex items-center justify-center h-screen font-bold text-4xl">
+      El Rincón Del Dev x Nuclio Digital School
+    </main>
+  );
+}
+
+export default App;
